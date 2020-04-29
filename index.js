22c3fb29ba00e85ddc475bb31a38373d2d37f8db
kapil jhajhria <jhajhria44@gmail.com>
2020-04-29 10:48:13 +0530

    getting customers list on /customers

--- index.js
+++ index.js
@@ -2,10 +2,22 @@ const express =require('express');
 const app =express();
 
 const port=5000;
+
+customer={
+    name:'kapil',
+    contactNo:'9812345678',
+    gender:'Male',
+    customerID:4212
+}
+let customers=[customer];
+
 app.listen(port,() => {
     console.log(`server is running on port${port}`);
 })
 
 app.get('/', function (req, res) {
     res.send('hello world')
+})
+app.get('/customers', function (req, res) {
+    res.send(customers)
 })
