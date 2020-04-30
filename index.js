b11ef3eeec5233eb665e82c15528c395e1561316
kapil jhajhria <jhajhria44@gmail.com>
2020-04-30 13:58:30 +0530

    add, delete and undo implemented

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const express =require('express');
-const app =express();
-const bodyParser     =        require("body-parser");
-const port=5000;
+const express = require('express');
+const app = express();
+const bodyParser = require("body-parser");
+const port = 5000;
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-customer={
-    name:'kapil',
-    contact:'98123456',
-    gender:'Male',
-    customerID:4212
+customer = {
+    name: 'kapil',
+    contact: '98123456',
+    gender: 'Male',
+    customerID: 4212
 }
-let customers=[customer];
-
+let customers = [customer];
+let deleteCustomers = [];
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header(
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
 });
 
 
-app.listen(port,() => {
+app.listen(port, () => {
     console.log(`server is running on port${port}`);
 })
 
@@ -36,61 +36,78 @@ app.get('/', function (req, res) {
 app.get('/customers', function (req, res) {
     res.send(customers)
 })
+app.get('/deletedCustomers', function (req, res) {
+    res.send(deleteCustomers.map((delCust) => delCust[0]))
+})
 //handling post request to server
 
 app.post('/', function (req, res) {
     console.log("got post request");
     console.log(req.body);
-    let tempcustomerID=Math.floor((Math.random() * 100) + 4000);
-    let tempCust={
-        name:req.body.name,
-        contact:req.body.contact,
-        gender:req.body.gender,
-        customerID:tempcustomerID
+    let tempcustomerID = Math.floor((Math.random() * 100) + 4000);
+    let tempCust = {
+        name: req.body.name,
+        contact: req.body.contact,
+        gender: req.body.gender,
+        customerID: tempcustomerID
     }
     customers.push(tempCust);
-    res.send({"customerID":tempcustomerID})
+    res.send({"customerID": tempcustomerID})
 })
 
 app.post('/deleteCustomer', function (req, res) {
-    console.log("got post request to delete customer : " +req.body.customerID);
+    console.log("got post request to delete customer : " + req.body.customerID);
     console.log(req.body);
-    let tempcustomerID=req.body.customerID;
-    let newCustomers=customers.filter((cust)=>cust.customerID!=tempcustomerID)
-    if(newCustomers.length!==customers.length){
-        customers=newCustomers;
-        res.send({status:"deleted",customerID:tempcustomerID})
-    }else{
-        res.send({status:"Not Deleted",customerID:tempcustomerID})
+    let tempcustomerID = req.body.customerID;
+    let deletedCustIndex = req.body.index;
+    deleteCustomers.push([customers.filter((cust) => cust.customerID == tempcustomerID)[0], deletedCustIndex, new Date().getTime()])
+    let newCustomers = customers.filter((cust) => cust.customerID != tempcustomerID)
+    if (newCustomers.length !== customers.length) {
+        customers = newCustomers;
+        res.send({status: "deleted", customerID: tempcustomerID})
+    } else {
+        res.send({status: "Not Deleted", customerID: tempcustomerID})
+    }
+})
+app.post('/undoDelete', function (req, res) {
+    console.log("got post request to delete customer : " + req.body.customerID);
+    console.log(req.body);
+    let tempcustomerID = req.body.customerID;
+    let custData = deleteCustomers.filter((cust) => cust[0].customerID == tempcustomerID)[0];
+    customers.push(custData[0])
+    if (custData.length !== 0) {
+        res.send({status: 'Found', deletedCustomer: custData[0], deletedCustomerIndex: custData[1]})
+    } else {
+        res.send({status: "notFound",})
     }
 })
 
 app.get('/customer/add', function (req, res) {
-    let tempCust={
-        name:'ap',
-        contact:Math.floor((Math.random() * 100) + 900000),
-        gender:'Male',
-        customerID:Math.floor((Math.random() * 100) + 4000)
+    let tempCust = {
+        name: 'ap',
+        contact: Math.floor((Math.random() * 100) + 900000),
+        gender: 'Male',
+        customerID: Math.floor((Math.random() * 100) + 4000)
     }
     customers.push(tempCust);
     res.send(tempCust)
 })
 
 app.get('/customer/:id', function (req, res) {
-    let customerID=req.params.id;
-    console.log("fetch details for customerID:"+customerID);
-    res.send(customers.filter((cust)=>cust.customerID==customerID))
+    let customerID = req.params.id;
+    console.log("fetch details for customerID:" + customerID);
+    res.send(customers.filter((cust) => cust.customerID == customerID))
 })
 
 app.get('/customer/delete/:id', function (req, res) {
-    let customerID=req.params.id;
-    console.log("deleting customerID:"+customerID);
-    let newCustomers=customers.filter((cust)=>cust.customerID!=customerID)
-    if(newCustomers.length!==customers.length){
-        customers=newCustomers;
-        res.send({status:"deleted",customerId:customerID})
-    }else{
-        res.send({status:"Not Deleted",customerId:customerID})
+    let customerID = req.params.id;
+    console.log("deleting customerID:" + customerID);
+    let newCustomers = customers.filter((cust) => cust.customerID != customerID)
+    if (newCustomers.length !== customers.length) {
+        customers = newCustomers;
+        res.send({status: "deleted", customerId: customerID})
+    } else {
+        res.send({status: "Not Deleted", customerId: customerID})
     }
 
 })
