55052e298947104582c0ca77b0939b7aa0bce4ad
kapiljhajhria <jhajhria44@gmail.com>
2021-01-12 13:33:03 +0530

    updated for aws lambda

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+// const serverless = require("serverless-http");
 const express = require("express");
 const winston = require("winston");
 
 const app = express();
+app.get("/", async (req, res) => {
+  console.log("get request made : test request");
+  return res.send("test api working");
+});
 require("./startup/logging")(); //logging errors
-require("./startup/routes")(app); //load the routes
+// require("./startup/routes")(app); //load the routes
 require("./startup/db")(); //connect to database
 require("./startup/config")(); //check for config values or env values. if not found throw error
 require("./startup/validation")(); //Joi related statements req for api endpoint events validation
@@ -13,4 +18,6 @@ const server = app.listen(port, () =>
   winston.info(`Listening on port ${port}...`)
 );
 
+module.exports = app;
 module.exports = server;
+// module.exports.handler = serverless(app);
