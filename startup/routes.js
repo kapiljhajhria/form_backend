00c792ae0837a018fa156ca1d6375ebcbf3e1f2f
kapiljhajhria <jhajhria44@gmail.com>
2021-01-12 13:30:56 +0530

    added test route

--- startup/routes.js
+++ startup/routes.js
@@ -3,11 +3,13 @@ const customers = require("../routes/customers");
 const users = require("../routes/users");
 const auth = require("../routes/auth");
 const error = require("../middleware/error");
+const test = require("../routes/test");
 
 module.exports = function (app) {
   app.use(express.json());
   app.use("/api/customers", customers);
   app.use("/api/users", users);
   app.use("/api/auth", auth);
+  app.use("/api/test", test);
   app.use(error);
 };
