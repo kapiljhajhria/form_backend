e44aa659cb06564d1d428019e33b4859184bb34f
kapil jhajhria <jhajhria44@gmail.com>
2021-01-12 17:56:12 +0530

    added back all routes

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get("/", async (req, res) => {
   return res.send("test api working");
 });
 require("./startup/logging")(); //logging errors
-// require("./startup/routes")(app); //load the routes
+require("./startup/routes")(app); //load the routes
 require("./startup/db")(); //connect to database
 require("./startup/config")(); //check for config values or env values. if not found throw error
 require("./startup/validation")(); //Joi related statements req for api endpoint events validation
